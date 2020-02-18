8231cecb42fc812f32cdd23056f81b4e1b833e24
Nitin Goyal <nigoyal@redhat.com>
2020-02-18 15:00:44 +0530

    Add delete a node from bst via iterative method

--- binary-search-tree.go
+++ binary-search-tree.go
@@ -46,6 +46,84 @@ func InsertNodeIterative(root * Node, data int) *Node {
     return root
 }
 
+func DeleteNodeIterative(root * Node, data int) *Node {
+    node := root
+    parent := root
+
+    // find element node and parent
+    for node != nil {
+        if node.data == data {
+            break
+        } else if node.data > data {
+            parent = node
+            node = node.left
+        } else if node.data < data {
+            parent = node
+            node = node.right
+        }
+    }
+
+    if node == nil {
+        return root
+    }
+
+    // if leaf node
+    if node.left == nil && node.right == nil {
+        if parent.left == node {
+            parent.left = nil
+        } else if parent.right == node {
+            parent.right = nil
+        } else if parent == node && root == node {
+            return nil
+        }
+    // if only left child
+    } else if node.left != nil && node.right == nil {
+        if parent.left == node {
+            parent.left = node.left
+            node.left = nil
+        } else if parent.right == node {
+            parent.right = node.left
+            node.left = nil
+        } else if parent == node && root == node {
+            node = node.left
+            return node
+        }
+    // if only right child
+    } else if node.left == nil && node.right != nil {
+        if parent.left == node {
+            parent.left = node.right
+            node.right = nil
+        } else if parent.right == node {
+            parent.right = node.right
+            node.right = nil
+        } else if parent == node && root == node {
+            node = node.right
+            return node
+        }
+    // if both childs are present
+    } else if node.left != nil && node.right != nil {
+        // get right most child of left sub tree
+        var successor * Node = node.left
+        var succ_parent * Node = node
+
+        for successor.right != nil {
+            succ_parent = successor
+            successor = successor.right
+        }
+
+        if succ_parent == node {
+            node.data = successor.data
+            node.left = successor.left
+            successor = nil
+        } else if succ_parent != node {
+            succ_parent.right = successor.left
+            successor.left = nil
+            node.data = successor.data
+        }
+    }
+    return root
+}
+
 func PreOrder(root * Node) {
     if root == nil {
         return
@@ -164,4 +242,14 @@ func main() {
     SearchIterative(root, 5)
     SearchIterative(root, 50)
     SearchIterative(root, 100)
+
+    fmt.Println("\nDelete")
+    DeleteNodeIterative(root, 95)
+    LevelOrderIterative(root)
+    fmt.Println()
+    DeleteNodeIterative(root, 90)
+    LevelOrderIterative(root)
+    fmt.Println()
+    DeleteNodeIterative(root, 50)
+    LevelOrderIterative(root)
 }
