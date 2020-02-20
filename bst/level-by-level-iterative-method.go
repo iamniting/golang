07dc2be2f1afc84b80fa514f92d1beeb2bcc527b
Nitin Goyal <nigoyal@redhat.com>
2020-02-20 16:26:01 +0530

    Add level by level order via iterative method

---
+++ bst/level-by-level-iterative-method.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data int
+	left *Node
+	right *Node
+}
+
+func InsertNodeRecursive(root *Node, data int) *Node {
+    if root == nil {
+        node := &Node{data, nil, nil}
+        return node
+    } else if  data < root.data {
+        root.left = InsertNodeRecursive(root.left, data)
+    } else if data > root.data {
+        root.right = InsertNodeRecursive(root.right, data)
+    }
+    return root
+}
+
+func LevelOrderIterative(root * Node) {
+    if root == nil { return }
+
+    var queue []*Node
+    queue = append(queue, root, nil)
+
+    for len(queue) > 0 {
+         if queue[0] == nil {
+            queue = queue[1:]
+            if len(queue) > 0 { queue = append(queue, nil) }
+            fmt.Println()
+            continue
+         }
+
+        parent := queue[0]
+        queue = queue[1:]
+
+        fmt.Print(parent.data, " ")
+        if parent.left != nil {
+            queue = append(queue, parent.left)
+        }
+        if parent.right != nil {
+            queue = append(queue, parent.right)
+        }
+    }
+}
+
+func main() {
+    slice := []int{50, 20, 80, 10, 30, 60, 90, 5, 15, 25, 35, 55, 65, 85, 95}
+
+    var root *Node
+
+    fmt.Println("Insert Nodes Recursively")
+    for _, element := range slice {
+        fmt.Print(element, " ")
+        root = InsertNodeRecursive(root, element)
+    }
+
+    fmt.Println("\n\nLevel By Level Traversal Iteratively")
+    LevelOrderIterative(root)
+}
