17b40bcb65945b41b094c1476f2ed6cd4c1d84a0
Nitin Goyal <nigoyal@redhat.com>
2020-02-21 13:41:05 +0530

    Add BitWise sol of is a given num odd

---
+++ bitwiseOperators/whether-a-no-is-odd.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func isNumberOdd(n int) bool {
+    // last bit will be always set for odd no
+    return true && (n & 1 == 1)
+}
+
+func main() {
+    for i := 0; i <= 16; i++ {
+        fmt.Println(i, isNumberOdd(i))
+    }
+}
