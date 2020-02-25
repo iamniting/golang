8e440e490ed09247b170337047a9d46bfa737dc7
Nitin Goyal <nigoyal@redhat.com>
2020-02-25 22:22:28 +0530

    Add sol of nodes with one child Recursively

---
+++ binarySearchTree/nodes-with-one-child-recursive-method.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+type Node struct {
+	data int
+	left *Node
+	right *Node
+}
+
+func InsertNodeRecursive(root *Node, data int) *Node {
+    if root == nil {
+        node := &Node{data, nil, nil}
+        return node
+    } else if  data < root.data {
+        root.left = InsertNodeRecursive(root.left, data)
+    } else if data > root.data {
+        root.right = InsertNodeRecursive(root.right, data)
+    }
+    return root
+}
+
+func HeightRecursive(root * Node) int {
+    if root == nil { return 0 }
+
+    return 1 + int(math.Max(
+        float64(HeightRecursive(root.left)),
+        float64(HeightRecursive(root.right))))
+}
+
+func LevelOrderRecursive(root * Node, level int) {
+    if root == nil { return }
+
+    if level == 1 && ((root.left != nil && root.right == nil) ||
+            (root.left == nil && root.right != nil)) {
+        fmt.Print(root.data, " ");
+        return
+    }
+
+    LevelOrderRecursive(root.left, level - 1)
+    LevelOrderRecursive(root.right, level - 1)
+}
+
+func LevelOrder(root * Node) {
+    height := HeightRecursive(root)
+
+    for h := 1; h <= height; h++ {
+        LevelOrderRecursive(root, h)
+    }
+}
+
+func main() {
+    slice := []int{10, 20, 30, 40, 50}
+
+    var root *Node
+
+    fmt.Println("Insert Nodes Recursively")
+    for _, element := range slice {
+        fmt.Print(element, " ")
+        root = InsertNodeRecursive(root, element)
+    }
+
+    fmt.Println("\n\nNodes with one child Recursively")
+    LevelOrder(root)
+}
