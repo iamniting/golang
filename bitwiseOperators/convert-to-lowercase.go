626d46726bf22ceb2effd8099527d43a77f5a647
Nitin Goyal <nigoyal@redhat.com>
2020-03-01 22:16:58 +0530

    Add trick convert char to lowercase via bitwiseOperators

---
+++ bitwiseOperators/convert-to-lowercase.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("%c\n", 'A' | ' ')
+    fmt.Printf("%c\n", 'B' | ' ')
+    fmt.Printf("%c\n", 'a' | ' ')
+    fmt.Printf("%c\n", 'b' | ' ')
+}
