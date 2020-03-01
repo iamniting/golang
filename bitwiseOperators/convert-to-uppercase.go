8040a0cab5ff6bc022e3a4a8123fdb2f9ea2058c
Nitin Goyal <nigoyal@redhat.com>
2020-03-01 22:19:17 +0530

    Add trick convert char to uppercase via bitwiseOperators

---
+++ bitwiseOperators/convert-to-uppercase.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("%c\n", 'A' & '_')
+    fmt.Printf("%c\n", 'B' & '_')
+    fmt.Printf("%c\n", 'a' & '_')
+    fmt.Printf("%c\n", 'b' & '_')
+}
