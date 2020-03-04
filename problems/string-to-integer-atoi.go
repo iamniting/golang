e8b0182a48a260a85f317e3f15003c14a129af8f
Nitin Goyal <nigoyal@redhat.com>
2020-03-04 16:17:23 +0530

    Add sol of String to Integer (atoi)

---
+++ problems/string-to-integer-atoi.go
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/string-to-integer-atoi
+// Just sol to the problem, It does not include the I/O part
+
+func myAtoi(str string) int {
+    res := 0
+    sign := 1
+    start := false
+
+    for _, s := range str {
+
+        if s >= '0' && s <= '9' {
+            res = (res * 10) + int(s - '0');
+            start = true
+            if res > math.MaxInt32 {
+                break
+            }
+        } else if !start && s == '-' {
+            start = true; sign = -1
+        } else if !start && s == '+' {
+            start = true; sign = +1
+        } else if !start && s == ' ' {
+            continue
+        } else {
+            break
+        }
+    }
+
+    res = res * sign
+
+    if res > math.MaxInt32 { return math.MaxInt32 }
+
+    if res < math.MinInt32 { return math.MinInt32 }
+
+    return res
+}
