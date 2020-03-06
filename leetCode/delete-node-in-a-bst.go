cc5039313ee7224d3931db47d0706543ca587cdd
Nitin Goyal <nigoyal@redhat.com>
2020-03-06 12:38:55 +0530

    Add sol of Delete Node in a BST

---
+++ leetCode/delete-node-in-a-bst.go
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/delete-node-in-a-bst
+// Just sol to the problem, It does not include the I/O part
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+    if root == nil {
+        return root
+    } else if root.Val > key {
+        root.Left = deleteNode(root.Left, key)
+    } else if root.Val < key {
+        root.Right = deleteNode(root.Right, key)
+    // if leaf node which need to be deleted
+    } else if root.Val == key && root.Left == nil && root.Right == nil {
+        return nil
+    // if node to be deleted having one or both child
+    } else {
+        // node with only left child
+        if root.Left == nil {
+            return root.Right
+        // node with only right child
+        } else if root.Right == nil {
+            return root.Left
+        }
+
+        // get max value of left sub tree
+        max := root.Left
+        for max.Right != nil { max = max.Right }
+
+        // replace root.Val with the max value
+        root.Val = max.Val
+
+        // delete max value of left sub tree
+        root.Left = deleteNode(root.Left, max.Val)
+    }
+    return root
+}
