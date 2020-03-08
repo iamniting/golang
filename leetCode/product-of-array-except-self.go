0d79aa866a9b575024c71b1f67d50fa08aaec453
Nitin Goyal <nigoyal@redhat.com>
2020-03-08 19:26:49 +0530

    Add sol of Product of Array Except Self

---
+++ leetCode/product-of-array-except-self.go
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/product-of-array-except-self
+// https://www.youtube.com/watch?v=khTiTSZ5QZY
+// Just sol to the problem, It does not include the I/O part
+
+func productExceptSelf(nums []int) []int {
+    res := []int{1}
+    left := 1
+    right := 1
+    l := len(nums)
+
+    // get product of all left elements and store it in res slice
+    for _, n := range nums[:l - 1] {
+        left *= n
+        res = append(res, left)
+    }
+
+    // get product of right elements and store it in right var
+    // change the val of res array by multiply left and right.
+    for i := l-1; i >= 0; i-- {
+        res[i] = res[i] * right
+        right *= nums[i]
+    }
+
+    return res
+}
