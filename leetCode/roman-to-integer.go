438b3252c8fd2ae567f852a766ef72c8ab1d649b
Nitin Goyal <nigoyal@redhat.com>
2020-03-10 12:20:53 +0530

    Add sol of Roman to Integer

---
+++ leetCode/roman-to-integer.go
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/roman-to-integer
+// Just sol to the problem, It does not include the I/O part
+
+func romanToInt(s string) int {
+
+    m := map[rune]int {
+        'I':1,
+	    'V':5,
+	    'X':10,
+	    'L':50,
+	    'C':100,
+	    'D':500,
+	    'M':1000,
+    }
+
+    res := 0
+
+    for i, c := range s {
+
+        // if next char is greater than the current one
+        if i < len(s)-1 && m[c] < m[rune(s[i+1])] {
+            res -= m[c]
+            continue
+        }
+
+        res += m[c]
+    }
+    return res
+}
