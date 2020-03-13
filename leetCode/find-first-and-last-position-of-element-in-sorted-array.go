350647c18f438ad4eb0ffb213aa1aa95e83937a8
Nitin Goyal <nigoyal@redhat.com>
2020-03-13 11:10:53 +0530

    Add sol of Find First and Last Position of Element in Sorted Array

---
+++ leetCode/find-first-and-last-position-of-element-in-sorted-array.go
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
+// Just sol to the problem, It does not include the I/O part
+
+func searchRange(nums []int, target int) []int {
+    res := []int{-1, -1}
+
+    // get left most element
+    res[0] = getLeft(nums, target)
+    // return if element not found
+    if res[0] == -1 { return res }
+    // get right most element
+    res[1] = getRight(nums, target)
+
+    return res
+}
+
+func getLeft(nums []int, target int) int {
+    res := -1
+    left, right := 0, len(nums) - 1
+
+    for left <= right {
+        mid := (left + right) >> 1
+
+        if nums[mid] >= target {
+            right = mid - 1
+        } else if nums[mid] <= target {
+            left = mid + 1
+        }
+
+        if nums[mid] == target {
+            res = mid
+        }
+    }
+    return res
+}
+
+func getRight(nums []int, target int) int {
+    res := -1
+    left, right := 0, len(nums) - 1
+
+    for left <= right {
+        mid := (left + right) >> 1
+
+        if nums[mid] <= target {
+            left = mid + 1
+        } else if nums[mid] >= target {
+            right = mid - 1
+        }
+
+        if nums[mid] == target {
+            res = mid
+        }
+    }
+    return res
+}
