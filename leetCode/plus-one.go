ff737ff3050a24386ae790bc63e7e10af5fa3dae
Nitin Goyal <nigoyal@redhat.com>
2020-03-14 23:10:50 +0530

    Add sol of Plus One

---
+++ leetCode/plus-one.go
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/plus-one
+// Just sol to the problem, It does not include the I/O part
+
+func plusOne(digits []int) []int {
+
+    for i:=len(digits)-1; i>=0; i-- {
+
+        if digits[i] < 9 {
+            digits[i]++
+            return digits
+        }
+        digits[i] = 0
+    }
+    digits = append([]int{1}, digits...)
+
+    return digits
+}
