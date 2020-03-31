f18158867520ce9297f8a51f5a8560f058d68f16
Nitin Goyal <nigoyal@redhat.com>
2020-03-31 22:35:21 +0530

    Add sol of Number of 1 Bits

---
+++ leetCode/number-of-1-bits.go
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/number-of-1-bits
+// Just sol to the problem, It does not include the I/O part
+
+func hammingWeight(num uint32) int {
+    res := 0
+
+    for num > 0 {
+        if num & 1 == 1 {
+            res++
+        }
+        num = num >> 1
+    }
+    return res
+}
