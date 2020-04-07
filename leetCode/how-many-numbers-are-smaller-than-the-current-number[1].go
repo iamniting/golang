282cc03d335827cdc7783ab870166a8bcd79ca90
Nitin Goyal <nigoyal@redhat.com>
2020-04-07 22:26:08 +0530

    Add O(n) sol of How Many Numbers Are Smaller Than the Current Number

---
+++ leetCode/how-many-numbers-are-smaller-than-the-current-number[1].go
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number
+// Just sol to the problem, It does not include the I/O part
+
+func smallerNumbersThanCurrent(nums []int) []int {
+
+    occurance := make([]int, 101)
+    res := make([]int, len(nums))
+
+    // get occurance of each num
+    for _, n := range nums {
+        occurance[n]++
+    }
+
+    // store count of lesser nums
+    for i:=1; i<101; i++ {
+        occurance[i] = occurance[i] + occurance[i-1]
+    }
+
+    // get result
+    for i, n := range nums {
+
+        if n == 0 {
+            res[i] = 0
+        } else {
+            res[i] = occurance[n-1]
+        }
+    }
+
+    return res
+}
