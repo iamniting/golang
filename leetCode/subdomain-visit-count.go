3faa5a652d3289a5dba93eaaf542316802efad95
Nitin Goyal <nigoyal@redhat.com>
2020-04-17 21:40:55 +0530

    Add sol of Subdomain Visit Count

---
+++ leetCode/subdomain-visit-count.go
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/subdomain-visit-count
+// Just sol to the problem, It does not include the I/O part
+
+func subdomainVisits(cpdomains []string) []string {
+
+    res := []string{}
+    m := make(map[string]int)
+
+    for _, str := range cpdomains {
+        // split domain and visits
+        slice := strings.Fields(str)
+        visits, domain := slice[0], "." + slice[1]
+
+        // create a map of domains and visits
+        for i:=len(domain)-1; i>=0; i-- {
+            if domain[i] == '.' {
+                key := domain[i+1:]
+                val, _ := strconv.Atoi(visits)
+                m[key] += val
+            }
+        }
+    }
+
+    // generate res from map
+    for k, v := range m {
+        res = append(res, strconv.Itoa(v) + " " + k)
+    }
+    return res
+}
