87bae1cb99b286194a1363d8346cd593fe548b69
Nitin Goyal <nigoyal@redhat.com>
2020-04-21 23:18:17 +0530

    Add sol of Minimum Value to Get Positive Step by Step Sum

---
+++ leetCode/minimum-value-to-get-positive-step-by-step-sum.go
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum
+// Just sol to the problem, It does not include the I/O part
+
+func minStartValue(nums []int) int {
+    res := 0
+
+    // run utill does not get ans
+    for res=1; res<=math.MaxInt32; res++ {
+        sum, flag := res, true
+
+        for _, n := range nums {
+            sum += n
+            if sum <= 0  {
+                flag = false
+                break
+            }
+        }
+        // if does not went into sum <= 0 state then return
+        if flag { return res }
+    }
+    return res
+}
