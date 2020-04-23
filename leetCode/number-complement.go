659946323155be25f7c885abda85496cf1078c75
Nitin Goyal <nigoyal@redhat.com>
2020-04-23 23:12:21 +0530

    Add sol of Number Complement

---
+++ leetCode/number-complement.go
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/number-complement
+// Just sol to the problem, It does not include the I/O part
+
+func findComplement(num int) int {
+
+    one := 0
+    // mark all bits 1 until i is 0
+    for i := num; i > 0; i >>= 1 {
+        one = one<<1 | 1
+    }
+    // xor the one and num e.g. 101 ^ 111 = 010
+    return one ^ num
+}
