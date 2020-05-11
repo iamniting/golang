9275d53b71dc6ff3a90fd2381bfbf95145f28695
Nitin Goyal <nigoyal@redhat.com>
2020-05-11 12:49:37 +0530

    Add efficient sol of Remove Outermost Parentheses

---
+++ leetCode/remove-outermost-parentheses[1].go
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/remove-outermost-parentheses
+// Just sol to the problem, It does not include the I/O part
+
+func removeOuterParentheses(S string) string {
+    start, index := 0, 0
+    res := ""
+
+    for i, c := range S {
+        if c == '(' {
+            index++
+        } else {
+            index--
+        }
+
+        if index == 0 {
+            res += S[start+1:i]
+            start = i+1
+        }
+    }
+    return res
+}
