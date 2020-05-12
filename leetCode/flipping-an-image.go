948912f4b43f0420f1ea267940e9495ae1bef089
Nitin Goyal <nigoyal@redhat.com>
2020-05-12 12:17:33 +0530

    Add sol of Flipping an Image

---
+++ leetCode/flipping-an-image.go
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/flipping-an-image
+// Just sol to the problem, It does not include the I/O part
+
+func flipAndInvertImage(A [][]int) [][]int {
+    res := make([][]int, len(A))
+
+    for i, row := range A {
+        res[i] = make([]int, len(row))
+
+        // reverse the row and flip the image
+        index := len(row) - 1
+        for _, num := range row {
+            res[i][index] = num ^ 1
+            index--
+        }
+    }
+    return res
+}
