be6b5911911a30833982ddd14e878534ef21f050
Nitin Goyal <nigoyal@redhat.com>
2020-05-13 13:09:55 +0530

    Add efficient sol of Self Dividing Numbers

---
+++ leetCode/self-dividing-numbers[1].go
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/self-dividing-numbers
+// Just sol to the problem, It does not include the I/O part
+
+func selfDividingNumbers(left int, right int) []int {
+
+    IsSelfDiv := func(n int) bool {
+        tmp := n
+        for tmp > 0 {
+            r := tmp % 10
+            tmp = tmp / 10
+            if r == 0 { return false }
+            if n % r != 0 { return false }
+        }
+        return true
+    }
+
+    res := []int{}
+    for i:=left; i<=right; i++ {
+        if IsSelfDiv(i) {
+            res = append(res, i)
+        }
+    }
+    return res
+}
