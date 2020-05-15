1df1659b24f26bfe5c3ac614d241a1e020b61fba
Nitin Goyal <nigoyal@redhat.com>
2020-05-15 11:17:08 +0530

    Add sol of Minimum Time Visiting All Points

---
+++ leetCode/minimum-time-visiting-all-points.go
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/minimum-time-visiting-all-points
+// Just sol to the problem, It does not include the I/O part
+
+func minTimeToVisitAllPoints(points [][]int) int {
+
+    getDiff := func(p1 []int, p2 []int) int {
+
+	    xDiff := math.Abs(float64(p1[0] - p2[0]))
+	    yDiff := math.Abs(float64(p1[1] - p2[1]))
+
+        return int(math.Max(xDiff, yDiff))
+    }
+
+	diff := 0
+	for i := 1; i < len(points); i++ {
+		diff += getDiff(points[i-1], points[i])
+	}
+	return diff
+}
