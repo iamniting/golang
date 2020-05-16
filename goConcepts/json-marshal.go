f9e48f125eb9aa02d537c552f7aaf7ce0a095536
Nitin Goyal <nigoyal@redhat.com>
2020-05-16 22:58:42 +0530

    Add eg. of json.MarshalIndent

---
+++ goConcepts/json-marshal.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type ServicePortName struct {
+	Name string
+	Port string
+	x    int
+}
+
+type ServiceEndpoint struct {
+	Endpoint        string
+	x               int
+	ServicePortName ServicePortName
+}
+
+func main() {
+	svc := ServiceEndpoint{"/api/users", 1, ServicePortName{"app.user", "9000", 2}}
+
+	s, _ := json.MarshalIndent(svc, "", "\t")
+	fmt.Println(string(s))
+	fmt.Printf("%+v", svc)
+}
+
