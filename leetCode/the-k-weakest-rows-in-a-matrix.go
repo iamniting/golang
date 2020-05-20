2605cc7388539f07cd5659c07be52ffec20fe254
Nitin Goyal <nigoyal@redhat.com>
2020-05-20 10:57:23 +0530

    Add sol of The K Weakest Rows in a Matrix

---
+++ leetCode/the-k-weakest-rows-in-a-matrix.go
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix
+// Just sol to the problem, It does not include the I/O part
+
+func kWeakestRows(mat [][]int, k int) []int {
+	type strong struct {
+		row, sol int
+	}
+	tmp := make([]strong, len(mat))
+	res := make([]int, k)
+
+	for i, row := range mat {
+		sol := 0
+		for _, col := range row {
+			sol += col
+		}
+		tmp[i] = strong{i, sol}
+	}
+
+	sort.Slice(tmp, func(i, j int) bool {
+		if tmp[i].sol == tmp[j].sol {
+			return tmp[i].row < tmp[j].row
+		}
+		return tmp[i].sol < tmp[j].sol
+	})
+
+	i := 0
+	for k > 0 {
+		res[i] = tmp[i].row
+		i++
+		k--
+	}
+	return res
+}
