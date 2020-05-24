e25ea24f548ac82416c852d796a66b877d6e3b8a
Nitin Goyal <nigoyal@redhat.com>
2020-05-24 22:31:27 +0530

    Add sol of Available Captures for Rook

---
+++ leetCode/available-captures-for-rook.go
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/available-captures-for-rook
+// Just sol to the problem, It does not include the I/O part
+
+func numRookCaptures(board [][]byte) int {
+
+	res := 0
+
+	for i, row := range board {
+		for j, col := range row {
+
+			if col == 'R' {
+				for k := j - 1; k >= 0; k-- {
+					if unicode.IsUpper(rune(row[k])) {
+						break
+					}
+					if row[k] == 'p' {
+						res++
+						break
+					}
+				}
+				for k := j + 1; k < 8; k++ {
+					if unicode.IsUpper(rune(row[k])) {
+						break
+					}
+					if row[k] == 'p' {
+						res++
+						break
+					}
+				}
+				for k := i - 1; k >= 0; k-- {
+					if unicode.IsUpper(rune(board[k][j])) {
+						break
+					}
+					if board[k][j] == 'p' {
+						res++
+						break
+					}
+				}
+				for k := i + 1; k < 8; k++ {
+					if unicode.IsUpper(rune(board[k][j])) {
+						break
+					}
+					if board[k][j] == 'p' {
+						res++
+						break
+					}
+				}
+				return res
+			}
+		}
+	}
+	return 0
+}
