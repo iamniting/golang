855383efa8678c457292bf7244e05a4204506013
Nitin Goyal <nigoyal@redhat.com>
2020-05-30 00:49:59 +0530

    Fmt problems programs with GoFmt

--- problems/count-pairs-from-two-arrays-having-sum-equal-to-k.go
+++ problems/count-pairs-from-two-arrays-having-sum-equal-to-k.go
@@ -5,33 +5,33 @@ package main
 import "fmt"
 
 func CountPairs(a1 []int, a2 []int, k int) int {
-    res := 0
-    // create a set
-    type void struct{}
-    var value void
-    set := make(map[int]void)
+	res := 0
+	// create a set
+	type void struct{}
+	var value void
+	set := make(map[int]void)
 
-    // create a set of elements of slice 1
-    for _, num := range a1 {
-        set[num] = value
-    }
+	// create a set of elements of slice 1
+	for _, num := range a1 {
+		set[num] = value
+	}
 
-    // check if (k - elements of slice 2) is present in set
-    for _, num := range a2 {
-        n := k - num
-        if _, ok := set[n]; ok {
-            res++
-        }
-    }
+	// check if (k - elements of slice 2) is present in set
+	for _, num := range a2 {
+		n := k - num
+		if _, ok := set[n]; ok {
+			res++
+		}
+	}
 
-    return res
+	return res
 
 }
 
 func main() {
-    var a1 = []int{1, 1, 3, 4, 5, 6, 6}
-    var a2 = []int{1, 4, 4, 5, 7}
-    var k int = 10
+	var a1 = []int{1, 1, 3, 4, 5, 6, 6}
+	var a2 = []int{1, 4, 4, 5, 7}
+	var k int = 10
 
-    fmt.Println(CountPairs(a1, a2, k))
+	fmt.Println(CountPairs(a1, a2, k))
 }
--- problems/sort-an-array-of-0s-and-1s.go
+++ problems/sort-an-array-of-0s-and-1s.go
@@ -3,24 +3,27 @@ package main
 import "fmt"
 
 func sortArray(arr []int) {
-    low := 0
-    high := len(arr) - 1
+	low := 0
+	high := len(arr) - 1
 
-    for low <= high {
+	for low <= high {
 
-        for arr[low] == 0 { low++ }
+		for arr[low] == 0 {
+			low++
+		}
 
-        for arr[high] == 1 { high-- }
+		for arr[high] == 1 {
+			high--
+		}
 
-        if low < high {
-            arr[low], arr[high] = arr[high], arr[low]
-        }
-    }
+		if low < high {
+			arr[low], arr[high] = arr[high], arr[low]
+		}
+	}
 }
 
 func main() {
-    slice := []int{0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0}
-    sortArray(slice)
-    fmt.Println(slice)
+	slice := []int{0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0}
+	sortArray(slice)
+	fmt.Println(slice)
 }
-
