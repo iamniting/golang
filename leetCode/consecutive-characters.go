8e083c08e31f251e43e53642627197f60a6815bf
Nitin Goyal <nigoyal@redhat.com>
2020-06-04 11:03:40 +0530

    Add sol of Consecutive Characters

---
+++ leetCode/consecutive-characters.go
@@ -0,0 +1,20 @@
+package main
+
+func maxPower(s string) int {
+
+	n, ans := 1, 1
+
+	for i := 1; i < len(s); i++ {
+
+		if s[i-1] == s[i] {
+			n++
+		} else {
+			n = 1
+		}
+
+		if n > ans {
+			ans = n
+		}
+	}
+	return ans
+}
