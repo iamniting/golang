27b2b098b2b9eec2df9c58dd53dbe4541bd6d3fb
Nitin Goyal <nigoyal@redhat.com>
2020-06-22 13:25:45 +0530

    Add sol of Construct String from Binary Tree

---
+++ leetCode/construct-string-from-binary-tree.go
@@ -0,0 +1,19 @@
+package main
+
+import "strconv"
+
+func tree2str(t *TreeNode) string {
+	if t == nil {
+		return ""
+	}
+	if t.Left == nil && t.Right == nil {
+		return strconv.Itoa(t.Val)
+	}
+	if t.Left != nil && t.Right == nil {
+		return strconv.Itoa(t.Val) + "(" + tree2str(t.Left) + ")"
+	}
+	if t.Left == nil && t.Right != nil {
+		return strconv.Itoa(t.Val) + "()" + "(" + tree2str(t.Right) + ")"
+	}
+	return strconv.Itoa(t.Val) + "(" + tree2str(t.Left) + ")" + "(" + tree2str(t.Right) + ")"
+}
