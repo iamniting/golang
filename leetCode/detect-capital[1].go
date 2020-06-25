6233d8ce1eda52b5270fe035b5dfb795916a0c15
Nitin Goyal <nigoyal@redhat.com>
2020-06-25 11:52:35 +0530

    Add efficient sol of Detect Capital

---
+++ leetCode/detect-capital[1].go
@@ -0,0 +1,9 @@
+package main
+
+import "strings"
+
+func detectCapitalUse(word string) bool {
+	return strings.ToLower(word) == word ||
+		strings.ToUpper(word) == word ||
+		strings.ToLower(word[1:]) == word[1:]
+}
