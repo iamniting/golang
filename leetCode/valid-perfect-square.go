fdfde14bfd1c84d9819819da5cd139d73bfa5021
Nitin Goyal <nigoyal@redhat.com>
2020-08-22 21:46:35 +0530

    Add sol of Valid Perfect Square

---
+++ leetCode/valid-perfect-square.go
@@ -0,0 +1,16 @@
+package main
+
+func isPerfectSquare(num int) bool {
+
+	if num == 1 {
+		return true
+	}
+
+	for i := 1; i <= num/2; i++ {
+		if i*i == num {
+			return true
+		}
+	}
+
+	return false
+}
