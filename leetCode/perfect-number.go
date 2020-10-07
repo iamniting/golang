3e923732ad6b0a4e518088a16722a953c71faab7
Nitin Goyal <nigoyal@redhat.com>
2020-10-07 22:10:14 +0530

    Add sol of Perfect Number

---
+++ leetCode/perfect-number.go
@@ -0,0 +1,17 @@
+package main
+
+func checkPerfectNumber(num int) bool {
+
+	var sum int
+
+	for i := 1; i <= num/2; i++ {
+		if num%i == 0 {
+			sum += i
+		}
+		if sum > num {
+			return false
+		}
+	}
+
+	return num == sum
+}
