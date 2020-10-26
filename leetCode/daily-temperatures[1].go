b1af93a3e9032a35da072145637c5843ce5e8b3c
Nitin Goyal <nigoyal@redhat.com>
2020-10-26 21:39:33 +0530

    Add efficient sol of Daily Temperatures

---
+++ leetCode/daily-temperatures[1].go
@@ -0,0 +1,19 @@
+package main
+
+func dailyTemperatures(T []int) []int {
+
+	var res, stack []int
+	res = make([]int, len(T))
+
+	for i := len(T) - 1; i >= 0; i-- {
+		for len(stack) > 0 && T[stack[len(stack)-1]] <= T[i] {
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack) > 0 {
+			res[i] = stack[len(stack)-1] - i
+		}
+		stack = append(stack, i)
+	}
+
+	return res
+}
