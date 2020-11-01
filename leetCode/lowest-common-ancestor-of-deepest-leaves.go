2450a5dc2fb5f79138193936362945b206ed8456
Nitin Goyal <nigoyal@redhat.com>
2020-11-01 20:54:46 +0530

    Add sol of Lowest Common Ancestor of Deepest Leaves

---
+++ leetCode/lowest-common-ancestor-of-deepest-leaves.go
@@ -0,0 +1,27 @@
+package main
+
+func lcaDeepestLeaves(root *TreeNode) *TreeNode {
+	_, res := postOrder(root)
+	return res
+}
+
+func postOrder(root *TreeNode) (int, *TreeNode) {
+
+	if root == nil {
+		return 0, nil
+	}
+
+	lh, left := postOrder(root.Left)
+	rh, right := postOrder(root.Right)
+
+	if lh > rh {
+		return lh + 1, left
+	}
+
+	if rh > lh {
+		return rh + 1, right
+	}
+
+	// if lh == rh means common ancestor
+	return rh + 1, root
+}
