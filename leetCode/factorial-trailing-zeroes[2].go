f343ba36ec86813ce66485e3671c66bee6b8cd62
Nitin Goyal <nigoyal@redhat.com>
2020-11-08 21:33:08 +0530

    Add efficient sol of Factorial Trailing Zeroes

---
+++ leetCode/factorial-trailing-zeroes[2].go
@@ -0,0 +1,21 @@
+package main
+
+func trailingZeroes(n int) int {
+
+	/*`
+	How many multiples of 5 are there in the numbers from 1 to 100?
+	because 100 ÷ 5 = 20, so, there are twenty multiples of 5 between 1 and 100.
+	but wait, actually 25 is 5×5, so each multiple of 25 has an extra factor of 5, e.g. 25 × 4 = 100，which introduces extra of zero.
+	So, we need know how many multiples of 25 are between 1 and 100? Since 100 ÷ 25 = 4, there are four multiples of 25 between 1 and 100.
+	Finally, we get 20 + 4 = 24 trailing zeroes in 100!
+	The above example tell us, we need care about 5, 5×5, 5×5×5, 5×5×5×5 ....
+	`*/
+
+	var res int
+
+	for i := 5; i <= n; i *= 5 {
+		res += n / i
+	}
+
+	return res
+}
