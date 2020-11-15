cc26d44c152d547383d9b283eae37b03e420994a
Nitin Goyal <nigoyal@redhat.com>
2020-11-15 22:35:47 +0530

    Add efficient sol of Defuse the Bomb

---
+++ leetCode/defuse-the-bomb[1].go
@@ -0,0 +1,30 @@
+package main
+
+func decrypt(code []int, k int) []int {
+
+	res := make([]int, len(code))
+
+	if k == 0 {
+		return res
+	}
+
+	start, end, sum := 1, k, 0
+
+	if k < 0 {
+		start, end, k = len(code)-(-k), len(code)-1, -k
+	}
+
+	for i := start; i <= end; i++ {
+		sum += code[i]
+	}
+
+	for i := range code {
+		res[i] = sum
+		sum -= code[start%len(code)]
+		start++
+		end++
+		sum += code[end%len(code)]
+	}
+
+	return res
+}
